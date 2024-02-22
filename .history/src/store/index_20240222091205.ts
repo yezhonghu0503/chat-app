5cb85601a9f2531ecd17372adb1fefd748713d54
yezhonghu0503 <211736726@qq.com>
2024-02-22 09:36:06 +0800

    fix:the Store export problem

--- .history/src/store/index_20240222091205.ts
+++ .history/src/store/index_20240222091205.ts
@@ -1,10 +1,10 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import mutualReducer from './reducers';
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
   counter: mutualReducer,
   // 有新的 slice 就往这里加
 });
-export const store: any = configureStore({
+const store: any = configureStore({
   reducer: rootReducer,
 });
