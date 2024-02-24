e718155a1b5ee3c0483c26a0112ed18837efd65e
yezhonghu0503 <211736726@qq.com>
2024-02-24 16:10:54 +0800

    feat:react-native-chart-kit引入&初始化图表

--- src/views/synthetical/UserMessage.tsx
+++ src/views/synthetical/UserMessage.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import {Image, StyleSheet, View, Text} from 'react-native';
+import {Image, StyleSheet, View, Text, Dimensions} from 'react-native';
+import {ProgressChart} from 'react-native-chart-kit';
 
 const UserMessage = () => {
+  const data = {
+    labels: ['文本生成Token', '文生图Token', 'xxx3'], // optional
+    data: [0.4, 0.6, 0.8],
+  };
+  const chartConfig = {
+    backgroundColor: '#875cff',
+    backgroundGradientFrom: '#875cff',
+    backgroundGradientTo: '#875cff',
+    decimalPlaces: 2, // optional, defaults to 2dp
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    propsForDots: {
+      r: '6',
+      strokeWidth: '2',
+      stroke: '#ffa726',
+    },
+  };
   return (
     <View style={styles.main}>
       <Image style={styles.userAvatar} source={require('./img/avatar.png')} />
       <Text style={styles.userName}>Hugo</Text>
+      <View style={styles.progressChartMain}>
+        <ProgressChart
+          data={data}
+          width={
+            Dimensions.get('window').width -
+            Dimensions.get('window').width * 0.6
+          }
+          height={150}
+          strokeWidth={10}
+          radius={20}
+          chartConfig={chartConfig}
+          hideLegend={true}
+          style={styles.progressChart}
+        />
+        <View style={styles.progressChartTake}>
+          <Text>123</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -23,6 +59,22 @@ const styles = StyleSheet.create({
   userName: {
     color: 'white',
     marginTop: 5,
+  },
+  progressChartMain: {
+    width: Dimensions.get('window').width,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 6,
+  },
+  progressChart: {
+    borderRadius: 15,
+  },
+  progressChartTake: {
+    width:
+      Dimensions.get('window').width - Dimensions.get('window').width * 0.5,
+    height: 150,
+    backgroundColor: '#875cff',
+    borderRadius: 15,
   },
 });
 
