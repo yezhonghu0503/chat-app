9c8db835cb144b4e644a061acd805eb7136aebca
yezhonghu0503 <211736726@qq.com>
2024-04-02 16:56:32 +0800

    fix:解决react-navigation依赖缺失问题

--- src/views/ContentOutput/index.tsx
+++ src/views/ContentOutput/index.tsx
@@ -3,14 +3,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import ChatBox from './chatbox/chatBox';
 import ImageGenerator from './imageGenerator/imgeGenerator';
+// import {View} from 'react-native';
 
 const Stack = createStackNavigator();
 const ContentOutput = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="chat">
-        <Stack.Screen name="chat" component={ChatBox} />
-        <Stack.Screen name="imageGener" component={ImageGenerator} />
+        <Stack.Screen
+          name="chat"
+          options={{headerShown: false}}
+          component={ChatBox}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="imageGener"
+          component={ImageGenerator}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
